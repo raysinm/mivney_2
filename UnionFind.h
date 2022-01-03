f420ee333d87a8f8c0ff72ab9febb235978d457a
stavtoledo <stavtoledo@campus.technion.ac.il>
2022-01-03 21:04:53 +0200

    uf changes

--- UnionFind.h
+++ UnionFind.h
@@ -22,34 +22,46 @@ namespace UF{
 
         class GroupNode{
             friend class UnionFind;
-            Data data; //*data holds AVL
-            int size;
+            //needs to hold groupNum?
+            GroupNode* father; //points to father if exists
             public:
-               GroupNode(Data data): data(data), size(1){}; //! data(data) could be dangerous- might copy the entire tree in O(n)
+               GroupNode(): father(nullptr){};
 
         };
 
-        GroupNode* groups;
+        class UnionGroup{
+            friend class UnionFind;
+            Data data; //data holds GroupData with AVL
+            int size; //holds num of groups
+            public:
+               UnionGroup(Data data): data(data), size(1){};
+        };
+
+        GroupNode* group_nodes;
+        UnionGroup* groups;
+        int num_of_groups;
 
         public:
-            explicit UnionFind(int k): size(k){
-                   groups = new int[k+1];
+               UnionFind(int k): num_of_groups(k){
+                   group_nodes = new GroupNode[k+1]
+                   groups = new UnionGroup[k+1];
                    for(int i=1; i<=k; i++){
-                       Data data_k; //create empty AVL 
-                       group_k = new GroupNode(data_k);
-                       groups[i] = group_k;
+                       groups[i].data(new Data); // ! legit?
+                       group_node_i = new GroupNode();
+                       group_nodes[i] = group_node_i;
                    }
                };
 
             void Union(int group1, int group2);
-
-            //TODO: find function that recieved group id and return a reference to the data of the associated GroupNode. needed for messing with the tree inside
+            UnionGroup Find(int group);
     };
 
     template<class Data>
     void Union(int group1, int group2){
-        GroupNode g1 = groups[group1];
-        GroupNode g2 = groups[group2];
+        GroupNode g1 = group_nodes[group1];
+        GroupNode g2 = group_nodes[group2];
+       // UnionGroup g1 = groups[group1];
+        //UnionGroup g2 = groups[group2];
 
         if(group1.group_size > group2.group_size)
         {
@@ -59,6 +71,17 @@ namespace UF{
             //in case more than one place in array points to group, how to change all to point at new group
 
         }
+    }
+
+    template<class Data>
+    UnionGroup Find(int group){
+        GroupNode* node = group_nodes[group];
+        while (node->father != nullptr)
+        {
+            node = node->father
+        }
+        
+
     }
 }
 
